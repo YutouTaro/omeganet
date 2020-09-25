220ed4edbdc7615927859741f95c55ed5161f429
yu <kxhyu17@gmail.com>
2020-09-25 14:03:50 +0800

    updates of training code

--- train.py
+++ train.py
@@ -16,10 +16,10 @@ now = datetime.now(timezone(timedelta(hours=8)))
 timestr = now.strftime("%y%m%d-%H%M%S")
 
 from options.option_train import TrainOptions
-opt = TrainOptions()
-opt = opt.parse()
+trainopt = TrainOptions()
+args = trainopt.parse()
 
-if opt.gpu_ids == -1:
+if args.gpu_ids == -1:
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 def configure_parameters():
@@ -29,23 +29,23 @@ def configure_parameters():
         :return testing_params: configuration for Tester
     """
     network_params = general_network.network_parameters(
-        height=opt.height,
-        width=opt.width,
-        load_only_baseline=opt.load_only_baseline,
-        tau=opt.tau,
+        height=args.height,
+        width=args.width,
+        load_only_baseline=args.load_only_baseline,
+        tau=args.tau,
     )
 
     dataloader_params = dataloader_parameters(
-        height=opt.height, width=opt.width, task=opt.task
+        height=args.height, width=args.width, task=args.task
     )
 
     testing_params = tester_factory.tester_parameters(
-        output_path=opt.dest,
-        checkpoint_path=opt.ckpt,
-        width=opt.width,
-        height=opt.height,
-        filenames_file=opt.filenames_file,
-        datapath=opt.datapath,
+        output_path=args.dest,
+        checkpoint_path=args.ckpt,
+        width=args.width,
+        height=args.height,
+        filenames_file=args.filenames_file,
+        datapath=args.datapath,
     )
 
     return network_params, dataloader_params, testing_params
@@ -60,9 +60,9 @@ def configure_network(network_params, dataloader_params):
 
     """
     training_flag = tf.placeholder(tf.bool)
-    dataloader = dataloader_factory.get_dataloader(opt.task)(
-        datapath=opt.datapath,
-        filenames_file=opt.filenames_file,
+    dataloader = dataloader_factory.get_dataloader(args.task)(
+        datapath=args.datapath,
+        filenames_file=args.filenames_file,
         params=dataloader_params,
     )
     batch = dataloader.get_next_batch()
@@ -74,16 +74,23 @@ def configure_network(network_params, dataloader_params):
     return network, dataloader, training_flag
 
 def main(_):
-    """Create the Dataloader, the Network and the Trainer.
-        Then, run the Trainer.
+    """Create the Dataloader, the Network and the Tester.
+        Then, run the Tester.
         :raise ValueError: if model does not exist
     """
-    model_exists = utilities.check_model_exists(opt.ckpt)
+    model_exists = utilities.check_model_exists(args.ckpt)
     if not model_exists:
         raise ValueError("Model not found")
     network_params, dataloader_params, testing_params = configure_parameters()
-    network, dataloader, training_flag = configure_network(network_params, dataloader_params)
+    print("=======dataloader_params: {}".format(dataloader_params))
+    network, dataloader, training_flag = configure_network(
+        network_params, dataloader_params
+    )
     print("=======training_flag: {}".format(training_flag))
+
+    # tester = tester_factory.get_tester(args.task)(testing_params)
+    # tester.test(network, dataloader, training_flag)
+
 
 if __name__ == "__main__":
     tf.app.run()
